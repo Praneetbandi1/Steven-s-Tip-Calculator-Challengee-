a10b515b5f513dd80c1b039f74cb67fcefe8988a
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-05-19 19:21:18 -0400

    added U #

---
+++ tipCalculator.js
@@ -0,0 +1,13 @@
+//U46368100
+function calTip(billAmount) {
+    tipAmount = (billAmount > 50 && billAmount <300) ? billAmount * 0.15 : billAmount * 0.20;
+    console.log("Bill: "+billAmount);
+    console.log("Tip: " + tipAmount);
+    
+totalCost=(tipAmount +billAmount);
+console.log("Total: "+ totalCost);
+
+
+}
+calTip(450);
+
