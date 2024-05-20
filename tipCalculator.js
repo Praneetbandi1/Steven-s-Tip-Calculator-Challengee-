62b1f00fc9c68581822a09b1cd94c1f1f3bf5ea3
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-05-19 20:27:15 -0400

    calculating tip using arrays

--- tipCalculator.js
+++ tipCalculator.js
@@ -3,11 +3,31 @@ function calTip(billAmount) {
     tipAmount = (billAmount > 50 && billAmount <300) ? billAmount * 0.15 : billAmount * 0.20;
     console.log("Bill: "+billAmount);
     console.log("Tip: " + tipAmount);
-    
-totalCost=(tipAmount +billAmount);
-console.log("Total: "+ totalCost);
+
+    totalCost=(tipAmount +billAmount);
+    console.log("Total: "+ totalCost);
+    return tipAmount;
+
+}
+//calTip(450);
+
+// bulk tip calculator
+function calTips(bills)
+{
+ //declaring  tips array   
+    tips= [];
+    for(i=0;i<bills.length;i++) {
+        tip=calTip(bills[i]);
+        console.log("Tipamount: "+i+" : "+ tip);
+
+        tips.push(tip);
+        
+    }
+
 
 
 }
-calTip(450);
 
+//calTips function calling
+calTips([275,40,430]);
+calTips([125,555,44]);
